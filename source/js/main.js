cd7e14dba6380761ea9bc1bd614d949beb2778c5
Polina Kuchina <Lermenua@yandex.ru>
2019-05-19 00:27:15 +0300

    Создает ховеры и фокусы

--- source/js/main.js
+++ source/js/main.js
@@ -1,35 +1,43 @@
-var navMain = document.querySelector('.main-navigation');
-window.addEventListener('DOMContentLoaded', function () {
-  navMain.classList.remove('main-navigation__button-menu--nojs');
-  navMain.classList.add('main-navigation__button-menu--closed');
-});
-document.querySelector('.main-navigation__toggle').addEventListener('click', function () {
-  if (navMain.classList.contains('main-navigation__button-menu--closed')) {
-    navMain.classList.remove('main-navigation__button-menu--closed');
-    navMain.classList.add('main-navigation__button-menu--opened');
+var main_navigation = document.querySelector('.main-navigation');
+var modal = document.querySelector('.modal');
+var overlay = document.querySelector('.modal__overlay');
+var product_hit__button = document.querySelector('.product-hit__button');
+var products__baskets = document.querySelectorAll('.products__basket');
+var main_navigation__toggle = document.querySelector('.main-navigation__toggle');
+
+function ShowMainNavigationHandler() {
+  if (main_navigation.classList.contains('main-navigation__button-menu--closed')) {
+    main_navigation.classList.remove('main-navigation__button-menu--closed');
+    main_navigation.classList.add('main-navigation__button-menu--opened');
   } else {
-    navMain.classList.add('main-navigation__button-menu--closed');
-    navMain.classList.remove('main-navigation__button-menu--opened');
+    main_navigation.classList.add('main-navigation__button-menu--closed');
+    main_navigation.classList.remove('main-navigation__button-menu--opened');
   }
-});
-document.body.addEventListener('click', function (event) {
-  var modal = document.querySelector('.modal');
-  var btn = document.querySelector('.product-hit__button');
-  var overlay = document.querySelector('.modal__overlay');
-  var target = event.target;
-  if (target !== modal && !modal.contains(target) || document.querySelector('.form-size__button') === target) {
-    modal.style.display = 'none';
-    overlay.classList.remove('modal__overlay--show')
+}
+
+function ShowModalHandler(event) {
+  event.preventDefault();
+  modal.style.display = 'block';
+  overlay.classList.add('modal__overlay--show');
+}
+
+function AddEventToProductBaskets(products__baskets) {
+  for(var i = 0; i < products__baskets.length; i++){
+    products__baskets[i].addEventListener('click' , ShowModalHandler);
   }
-  if (target === btn) {
-    event.preventDefault();
-    modal.style.display = 'block';
-    overlay.classList.add('modal__overlay--show')
+}
+
+function Initialization(){
+  if(main_navigation){
+    main_navigation.classList.remove('main-navigation__button-menu--nojs');
+    main_navigation.classList.add('main-navigation__button-menu--closed');
   }
-});
-window.addEventListener('resize', function () {
-  if (navMain.classList.contains('main-navigation--opened')) {
-    navMain.classList.remove('main-navigation--opened');
-    navMain.classList.add('main-navigation--closed');
-  }
-});
+  if(product_hit__button) product_hit__button.addEventListener('click' , ShowModalHandler);
+  if(products__baskets) AddEventToProductBaskets(products__baskets);
+  if(main_navigation__toggle) main_navigation__toggle.addEventListener('click', ShowMainNavigationHandler);
+}
+
+Initialization();
+
+
+
